4af2a72ea94a4b8f3d60377b1599ad9fbfada130
SergiyIvan <serhii.ivanenko@oracle.com>
2020-08-16 20:24:04 +0300

    Added Type property to Node

--- Crosslight.API/Nodes/Node.cs
+++ Crosslight.API/Nodes/Node.cs
@@ -7,6 +7,7 @@ namespace Crosslight.API.Nodes
     {
         public Node Parent { get; protected set; } = null;
         public IEnumerable<Node> Children { get; protected set; } = null;
+        public Type Type { get => GetType(); }
         public override string ToString()
         {
             return "Node";
