acecb967b65dcbd43cbe695a9f4418dfbd25f403
Mykola Morozov <mcmikecreations@gmail.com>
2021-06-21 02:26:00 +0300

    API: Added unary and binary expression constants, literals.

---
+++ src/Crosslight.API/Nodes/Expressions/LiteralExpressionKinds.cs
@@ -0,0 +1,55 @@
+﻿namespace Crosslight.API.Nodes.Expressions
+{
+    public static class LiteralExpressionKinds
+    {
+        /// <summary>
+        /// Variable argument list (VarArg)
+        /// </summary>
+        public const string ArgListLiteral = "al";
+        /// <summary>
+        /// Any integer, fixed- or floating-point decimal
+        /// </summary>
+        public const string NumericLiteral = "nm";
+        /// <summary>
+        /// Non-interpolated string
+        /// </summary>
+        public const string StringLiteral = "st";
+        /// <summary>
+        /// A single separate character from a string
+        /// </summary>
+        public const string CharacterLiteral = "cr";
+        /// <summary>
+        /// True value
+        /// </summary>
+        public const string BooleanTrueLiteral = "bt";
+        /// <summary>
+        /// False value
+        /// </summary>
+        public const string BooleanFalseLiteral = "bf";
+        /// <summary>
+        /// Null value
+        /// </summary>
+        public const string NullLiteral = "nl";
+        /// <summary>
+        /// Default value, which will be deduced during compilation
+        /// </summary>
+        public const string DefaultLiteral = "df";
+
+        /// <summary>
+        /// Long integer or decimal
+        /// </summary>
+        public const string LongLiteral = "lg";
+        /// <summary>
+        /// Short integer or decimal
+        /// </summary>
+        public const string ShortLiteral = "st";
+        /// <summary>
+        /// Signed integer
+        /// </summary>
+        public const string SignedLiteral = "sn";
+        /// <summary>
+        /// Unsigned integer
+        /// </summary>
+        public const string UnsignedLiteral = "un";
+    }
+}
