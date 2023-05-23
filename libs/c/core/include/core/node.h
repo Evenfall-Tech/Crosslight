5ee7614745c251f335c2f5c7fc4a7aa0cdb2d340
Mykola Morozov <mcmikecreations@gmail.com>
2023-05-23 21:02:15 +0200

    Verified symbol export and cpp interop.
    Signed-off-by: Mykola Morozov <mcmikecreations@gmail.com>

--- libs/c/core/include/core/node.h
+++ libs/c/core/include/core/node.h
@@ -1,7 +1,16 @@
 #pragma once
 
+#include <stdlib.h>
 #include "core/definitions.h"
 
-EXPORT struct cl_node {
-    struct cl_node* parent;
+/**
+ * @brief Base type for language node trees.
+*/
+struct cl_node {
+    void* payload; /**< Payload node containing specialized fields. */
+    struct cl_node* parent; /**< Parent of the current node. Used only for faster traversing. */
+    struct cl_node* children;
+    size_t child_count;
 };
+
+size_t EXPORT cl_node_append(struct cl_node* parent, struct cl_node* child);
