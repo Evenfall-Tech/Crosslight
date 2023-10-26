a31f2c8a6240feb36ddb794fb329149c39d047e9
Mykola Morozov <mykola.morozov@tum.de>
2023-10-26 16:04:44 +0200

    WIP: added more test cases.

--- tests/src/lang/builders/source_root.cpp
+++ tests/src/lang/builders/source_root.cpp
@@ -2,6 +2,7 @@
 #include "lang/builders/allocator.hpp"
 #include "lang/builders/builder.hpp"
 #include "core/node.h"
+#include "core/nodes/node_type.h"
 #include "doctest/doctest.h"
 
 namespace b = cl::lang::builders;
@@ -31,7 +32,7 @@ TEST_CASE("Hierarchy temporary") {
     REQUIRE_NE(root, factory.parent_get());
     REQUIRE_EQ(1, root->child_count);
     REQUIRE_UNARY(root->payload);
-    REQUIRE_UNARY(root->payload_type);
+    REQUIRE_EQ(::source_root, root->payload_type);
     REQUIRE_UNARY_FALSE(root->parent);
 
     auto child = root->children;
@@ -40,7 +41,7 @@ TEST_CASE("Hierarchy temporary") {
     REQUIRE_EQ(0, child->child_count);
     REQUIRE_UNARY_FALSE(child->children);
     REQUIRE_UNARY(child->payload);
-    REQUIRE_UNARY(child->payload_type);
+    REQUIRE_EQ(::source_root, child->payload_type);
     REQUIRE_EQ(root, child->parent);
 }
 
@@ -54,7 +55,7 @@ TEST_CASE("Hierarchy variable") {
     REQUIRE_NE(root, factory.parent_get());
     REQUIRE_EQ(1, root->child_count);
     REQUIRE_UNARY(root->payload);
-    REQUIRE_UNARY(root->payload_type);
+    REQUIRE_EQ(::source_root, root->payload_type);
     REQUIRE_UNARY_FALSE(root->parent);
 
     auto child = root->children;
@@ -63,7 +64,7 @@ TEST_CASE("Hierarchy variable") {
     REQUIRE_EQ(0, child->child_count);
     REQUIRE_UNARY_FALSE(child->children);
     REQUIRE_UNARY(child->payload);
-    REQUIRE_UNARY(child->payload_type);
+    REQUIRE_EQ(::source_root, child->payload_type);
     REQUIRE_EQ(root, child->parent);
 }
 
@@ -77,7 +78,7 @@ TEST_CASE("Hierarchy variable rewrite") {
     REQUIRE_NE(root, factory.parent_get());
     REQUIRE_EQ(1, root->child_count);
     REQUIRE_UNARY(root->payload);
-    REQUIRE_UNARY(root->payload_type);
+    REQUIRE_EQ(::source_root, root->payload_type);
     REQUIRE_UNARY_FALSE(root->parent);
 
     auto child = root->children;
@@ -86,8 +87,101 @@ TEST_CASE("Hierarchy variable rewrite") {
     REQUIRE_EQ(0, child->child_count);
     REQUIRE_UNARY_FALSE(child->children);
     REQUIRE_UNARY(child->payload);
-    REQUIRE_UNARY(child->payload_type);
+    REQUIRE_EQ(::source_root, child->payload_type);
     REQUIRE_EQ(root, child->parent);
+}
+
+TEST_CASE("Hierarchy temporary nested temporary") {
+    auto m = b::allocator{ malloc, free };
+    auto factory = m.source_root("hello.ts") << m.source_root("world.ts") << m.source_root("!.ts");
+
+    auto root = factory.root_get();
+    REQUIRE_UNARY(root);
+    REQUIRE_NE(root, factory.parent_get());
+    REQUIRE_EQ(1, root->child_count);
+    REQUIRE_UNARY(root->payload);
+    REQUIRE_EQ(::source_root, root->payload_type);
+    REQUIRE_UNARY_FALSE(root->parent);
+
+    auto child = root->children;
+    REQUIRE_UNARY(child);
+    REQUIRE_NE(child, factory.parent_get());
+    REQUIRE_EQ(1, child->child_count);
+    REQUIRE_UNARY(child->payload);
+    REQUIRE_EQ(::source_root, child->payload_type);
+    REQUIRE_EQ(root, child->parent);
+
+    child = child->children;
+    REQUIRE_UNARY(child);
+    REQUIRE_EQ(child, factory.parent_get());
+    REQUIRE_EQ(0, child->child_count);
+    REQUIRE_UNARY_FALSE(child->children);
+    REQUIRE_UNARY(child->payload);
+    REQUIRE_EQ(::source_root, child->payload_type);
+    REQUIRE_EQ(root->children, child->parent);
+}
+
+TEST_CASE("Hierarchy variable nested temporary") {
+    auto m = b::allocator{ malloc, free };
+    auto factory = m.source_root("hello.ts");
+    factory = factory << m.source_root("world.ts") << m.source_root("!.ts");
+
+    auto root = factory.root_get();
+    REQUIRE_UNARY(root);
+    REQUIRE_NE(root, factory.parent_get());
+    REQUIRE_EQ(1, root->child_count);
+    REQUIRE_UNARY(root->payload);
+    REQUIRE_EQ(::source_root, root->payload_type);
+    REQUIRE_UNARY_FALSE(root->parent);
+
+    auto child = root->children;
+    REQUIRE_UNARY(child);
+    REQUIRE_NE(child, factory.parent_get());
+    REQUIRE_EQ(1, child->child_count);
+    REQUIRE_UNARY(child->payload);
+    REQUIRE_EQ(::source_root, child->payload_type);
+    REQUIRE_EQ(root, child->parent);
+
+    child = child->children;
+    REQUIRE_UNARY(child);
+    REQUIRE_EQ(child, factory.parent_get());
+    REQUIRE_EQ(0, child->child_count);
+    REQUIRE_UNARY_FALSE(child->children);
+    REQUIRE_UNARY(child->payload);
+    REQUIRE_EQ(::source_root, child->payload_type);
+    REQUIRE_EQ(root->children, child->parent);
+}
+
+TEST_CASE("Hierarchy variable nested variable") {
+    auto m = b::allocator{ malloc, free };
+    auto factory = m.source_root("hello.ts");
+    factory = factory << m.source_root("world.ts");
+    factory = factory << m.source_root("!.ts");
+
+    auto root = factory.root_get();
+    REQUIRE_UNARY(root);
+    REQUIRE_NE(root, factory.parent_get());
+    REQUIRE_EQ(1, root->child_count);
+    REQUIRE_UNARY(root->payload);
+    REQUIRE_EQ(::source_root, root->payload_type);
+    REQUIRE_UNARY_FALSE(root->parent);
+
+    auto child = root->children;
+    REQUIRE_UNARY(child);
+    REQUIRE_NE(child, factory.parent_get());
+    REQUIRE_EQ(1, child->child_count);
+    REQUIRE_UNARY(child->payload);
+    REQUIRE_EQ(::source_root, child->payload_type);
+    REQUIRE_EQ(root, child->parent);
+
+    child = child->children;
+    REQUIRE_UNARY(child);
+    REQUIRE_EQ(child, factory.parent_get());
+    REQUIRE_EQ(0, child->child_count);
+    REQUIRE_UNARY_FALSE(child->children);
+    REQUIRE_UNARY(child->payload);
+    REQUIRE_EQ(::source_root, child->payload_type);
+    REQUIRE_EQ(root->children, child->parent);
 }
 
 TEST_SUITE_END();
